dc860af9afcc95ed0ba213bfcb2f1f01fd7e2729
Shashank Sharma <shashankrnr32@gmail.com>
2023-05-22 11:52:25 +0530

    Fix mypy Check

--- pycommons/lang/__init__.py
+++ pycommons/lang/__init__.py
@@ -15,6 +15,6 @@ __email__ = "shashankrnr32@gmail.com"
 # Used to automatically set version number from github actions
 # as well as not break when being tested locally
 try:
-    __version__ = version(__package__)  # type: ignore
+    __version__ = version(__package__)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0"
