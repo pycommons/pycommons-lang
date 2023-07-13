8e1a656b693ab57d3e71628c92877c124bca489a
Shashank Sharma <shashankrnr32@gmail.com>
2023-07-13 22:36:10 +0530

    Add Tuple classes to pycommons-lang

---
+++ pycommons/lang/tuple/__init__.py
@@ -0,0 +1,4 @@
+from .pair import Pair, ImmutablePair, MutablePair
+from .triple import Triple, ImmutableTriple, MutableTriple
+
+__all__ = ["Pair", "ImmutablePair", "MutablePair", "Triple", "ImmutableTriple", "MutableTriple"]
